d5dcd6c6f7b2c56fbfb95a04b9104e7779c949be
devinkbrown <dbrown4185@student.gwinnetttech.edu>
2026-06-19 14:40:48 +0200

    chore(ruri): remove old React/Next blueprint tree (app/ components/ hooks/ lib/ __tests__/ + Next configs)
    - the Solid app under src/ is self-contained; old tree was reference-only since wave 1
    - announce-bot component/LOC counts now reflect the real Solid codebase

--- components/stream/index.ts
+++
@@ -1,5 +0,0 @@
-export { GoLiveModal } from './GoLiveModal';
-export { StreamLayout } from './StreamLayout';
-export { StreamOverlay } from './StreamOverlay';
-export { RaidBanner } from './RaidBanner';
-export { PollWidget, PollCreateModal } from './PollWidget';
